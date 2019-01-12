ebcf803b99cb3302839af3652791359d4939751c
sonhoang0611 <hoangthaison985@gmail.com>
2019-01-13 05:52:38 +0700

    Change text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Le Thi My Duyen"
+    render html: "Hen gap lai"
   end
 end
